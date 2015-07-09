9fb104a121f7077a13fa2060c50098c46801a3cc
Toon Ketels <toon.ketels@gmail.com>
2015-07-09 13:29:22 +0200

    List available templates.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,39 @@
 module Main where
 
-main = putStrLn "Hello world"
+import System.FilePath
+import System.Directory
+import Control.Monad
+
+
+templatesDir :: FilePath
+templatesDir =  ".gitignore"
+
+extension    :: String
+extension    =  "gitignore"
+
+
+listAvailableTemplates :: IO [FilePath]
+listAvailableTemplates = do
+    home  <- getHomeDirectory
+    files <- getDirectoryContents $ home </> templatesDir
+    return $ filter isTemplate files
+        where isTemplate   :: FilePath -> Bool
+              isTemplate p =  takeExtension p == '.' : extension
+
+
+getFullPathTemplate :: FilePath -> FilePath -> FilePath
+getFullPathTemplate home file = home </> templatesDir </> file
+
+
+getFullPathTemplates :: FilePath -> [FilePath] -> [FilePath]
+getFullPathTemplates home = map (getFullPathTemplate home)
+
+
+main :: IO ()
+main = do
+    home      <- getHomeDirectory
+    templates <- listAvailableTemplates
+    let full = getFullPathTemplates home templates
+    forM_ full putStrLn
+    return ()
+
